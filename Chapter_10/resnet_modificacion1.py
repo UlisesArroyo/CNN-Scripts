c022d99d6165acd53b524136e0ef0dda4533482c
UlisesArroyo <ulisesdanarroyorojas@gmail.com>
2022-05-18 12:48:38 -0500

    se agrego el tiempo y txt

--- Chapter_10/resnet_modificacion1.py
+++ Chapter_10/resnet_modificacion1.py
@@ -7,7 +7,7 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader, Dataset
 from torchvision import datasets, models, transforms
 import numpy as np
-import time
+from time import time
 import os
 
 
@@ -19,9 +19,11 @@ learning_rate = 0.001
 batch_size = 64
 num_epoch = 5
 
+name = "modficacion_1"
+os.makedirs('./'+"modificaciones", exist_ok=True)
+
 
 dataset_path = './datasets/cartoon_face/'
-
 
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
@@ -62,9 +64,9 @@ dataloaders = {
                                 num_workers=0)  # for Kaggle
 }
 
-#Load de model
+#Se carga el modelo
 model = models.resnet50(pretrained=True).to(device)
-    
+#Se desabilita el aprendizaje en todas las capas
 for param in model.parameters():
     param.requires_grad = False   
     
@@ -76,16 +78,22 @@ model.fc = nn.Sequential(
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.fc.parameters())
 
+time1 = time()     
+
 train_accuracy = []
 train_loss = []
 
 val_accuracy = []
 val_loss = []
 
-def train_model(model, criterion, optimizer, num_epochs=3):
+def train_model(model, criterion, optimizer, num_epochs):
     for epoch in range(num_epochs):
+        file = open("./" + "modificaciones/" + name + ".txt", "a+")
+        file.write("Epoch " + str(epoch+1) +"/" + num_epoch + "\n")
+	    file.close()
         print('Epoch {}/{}'.format(epoch+1, num_epochs))
         print('-' * 10)
+        time0 = time()    
 
         for phase in ['train', 'test']:
             if phase == 'train':
@@ -114,19 +122,27 @@ def train_model(model, criterion, optimizer, num_epochs=3):
             
             epoch_loss = running_loss / len(image_datasets[phase])
             epoch_acc = running_corrects.double() / len(image_datasets[phase])
-
+            file = open("./" + "modificaciones/" + name + ".txt", "a+")
+            file.write('{} loss: {:.4f}, acc: {:.4f}'.format(phase, epoch_loss, epoch_acc)+"\n")
+            file.write("time: {:.4f} seg".format(time()-time0)+"\n")
+	        file.close()
             print('{} loss: {:.4f}, acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
-
+            print("time: {:.4f} seg".format(time()-time0))
+            time2 = time()   
             if phase == 'train':
                 train_accuracy.append(epoch_acc)
                 train_loss.append(loss)
             else:
                 val_accuracy.append(epoch_acc)
                 val_loss.append(epoch_loss)
+        file = open("./" + "modificaciones/" + name + ".txt", "a+")
+        file.write("\nTraining Time (in minutes) =",(time2-time1)/60 + "\n")
+        file.close
+        print("\nTraining Time (in minutes) =",(time2-time1)/60)
 
     return model
 
-model_trained = train_model(model, criterion, optimizer, num_epochs=30)
+model_trained = train_model(model, criterion, optimizer, num_epochs)
 
 def getAccuracyLossTrain():
     return train_accuracy, train_loss
@@ -134,21 +150,19 @@ def getAccuracyLossTrain():
 def getAccuracyLossVal():
     return val_accuracy, val_loss
 
-if __name__ == "__main__":
-    epochs = 30
-    batch_size = 64
 
-    epochs = range(1, epochs + 1, 1)
-
-    vgg_t_t_a, vgg_t_t_l = getAccuracyLossTrain()
-
-    print('Torch train')
-    print('Maximo valor de accuracy: {} Ultimo valor de accuracy: {}'.format(max(vgg_t_t_a), vgg_t_t_a[len(vgg_t_t_a) - 1]))
-    print('Menor valor de loss: {} Ultimo valor de loss: {}'.format(min(vgg_t_t_l), vgg_t_t_l[len(vgg_t_t_l) - 1]))
-
-    vgg_t_v_a, vgg_t_v_l = getAccuracyLossVal()
+epochs = range(1, epochs + 1, 1)
 
 
-    print('Torch Val')
-    print('Maximo valor de accuracy: {} Ultimo valor de accuracy: {}'.format(max(vgg_t_v_a), vgg_t_v_a[len(vgg_t_v_a) - 1]))
-    print('Menor valor de loss: {} Ultimo valor de loss: {}'.format(min(vgg_t_v_l), vgg_t_v_l[len(vgg_t_v_l) - 1]))
+vgg_t_t_a, vgg_t_t_l = getAccuracyLossTrain()
+
+print('Torch train')
+print('Maximo valor de accuracy: {} Ultimo valor de accuracy: {}'.format(max(vgg_t_t_a), vgg_t_t_a[len(vgg_t_t_a) - 1]))
+print('Menor valor de loss: {} Ultimo valor de loss: {}'.format(min(vgg_t_t_l), vgg_t_t_l[len(vgg_t_t_l) - 1]))
+
+vgg_t_v_a, vgg_t_v_l = getAccuracyLossVal()
+
+
+print('Torch Val')
+print('Maximo valor de accuracy: {} Ultimo valor de accuracy: {}'.format(max(vgg_t_v_a), vgg_t_v_a[len(vgg_t_v_a) - 1]))
+print('Menor valor de loss: {} Ultimo valor de loss: {}'.format(min(vgg_t_v_l), vgg_t_v_l[len(vgg_t_v_l) - 1]))
